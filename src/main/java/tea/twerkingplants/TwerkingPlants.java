0c8ffa1132da349ab6197fb78efd628fd6f541b7
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-10-28 17:30:37 +0100

    setup

---
+++ src/main/java/tea/twerkingplants/TwerkingPlants.java
@@ -0,0 +1,17 @@
+package tea.twerkingplants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TwerkingPlants extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
